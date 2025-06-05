d47107b8d7462ce8d850b3a3a714a01c49d13817
Md. Ibtihazzaman <148910683+Sadid19@users.noreply.github.com>
2025-06-06 00:20:34 +0600

    Today's first problem solved!

---
+++ Codeforce Group Contest Problems/Contest #1/Problem(C)_Next_Alphabet.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch = 'a';
+    cin >> ch;
+
+   if(ch == 'z')
+   {
+    cout << 'a' << endl;
+   }
+   else
+   {
+       ch++;
+       cout << ch << endl;
+   }
+}
