eafe6b1f06e8f3968739ee4772e4e95d39950d7b
Md. Ibtihazzaman <148910683+Sadid19@users.noreply.github.com>
2025-06-10 15:15:20 +0600

    Today's problem solved!

---
+++ Codeforce Group Contest Problems/Sheet #2 (Loops)/219432(C)_Even_Odd_Positive_and_Negative.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int even = 0;
+    int odd = 0;
+    int positive = 0;
+    int negative = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+
+        if(num % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+        if(num > 0)
+        {
+            positive ++;
+        }
+        else if(num < 0)
+        {
+            negative++;
+        }
+    }
+    cout << "Even: " << even << endl;
+    cout << "Odd: " << odd << endl;
+    cout << "Positive: " << positive << endl;
+    cout << "Negative: " << negative << endl;
+}
