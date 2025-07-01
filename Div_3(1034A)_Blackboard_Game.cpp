5b1629e7312cf1acab426f901b0afb2fe3e5a7ba
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-07-01 21:51:34 +0600

    6th contest!

---
+++ Div_3(1034A)_Blackboard_Game.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        if(n % 4 == 0)
+        {
+            cout << "Bob \n";
+        }
+        else
+        {
+            cout << "Alice \n";
+        }
+    }
+
+
+}
