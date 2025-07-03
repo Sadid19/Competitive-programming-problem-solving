3244ae3c8a13e2b825ccdcff8dd974ea7094bd81
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-07-03 22:57:17 +0600

    Another problem solved!

---
+++ 1(A)_Theatre_Square.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ll m, n, a;
+    cin >> m >> n >> a;
+
+    ll flagstone_n = (n + a - 1) / a;
+    ll flagstone_m = (m + a - 1) / a;
+
+    cout << flagstone_n * flagstone_m << endl;
+}
