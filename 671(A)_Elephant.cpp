44a9d4045cd9325b515fb2fbb3eb6237939a1e01
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-07-12 16:41:09 +0600

    Today's problem solved!

---
+++ 671(A)_Elephant.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int step = n / 5;
+
+    if(n % 5 != 0)
+    {
+        step++;
+    }
+
+    cout << step << endl;
+}
