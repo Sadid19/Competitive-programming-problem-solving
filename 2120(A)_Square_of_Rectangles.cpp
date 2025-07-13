a9113263c766599c566c69080353cf30998ddecd
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-07-14 00:28:17 +0600

    Update 2120(A)_Square_of_Rectangles.cpp

--- 2120(A)_Square_of_Rectangles.cpp
+++ 2120(A)_Square_of_Rectangles.cpp
@@ -10,11 +10,11 @@ int main()
         int l1 , l2, l3, b1, b2, b3;
         cin >> l1 >> b1 >> l2 >> b2 >> l3 >> b3;
 
-        if(l1 == l2 && l1 == l3 && b1 + b2 == l1)
+        if(l1 == l2 && l1 == l3 && b1 + b2 + b3 == l1)
         {
             cout << "YES \n";
         }
-        else if(l1 == (l2 + l3) && b2 == b3 && l1 + l2 == b1)
+        else if(l1 == (l2 + l3) && b2 == b3 && b1 + b2 == l1)
         {
             cout << "YES \n";
         }
