02164d0b469cb9138452f50ddfd831450430f9cf
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-07-25 15:34:47 +0600

    Today's problem solved!

---
+++ 913(A)_Modular_Exponentiation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll n, m;
+    cin >> n >> m;
+
+    ll a = 1;
+    if(n >= 30)
+    {
+        cout << m << endl;
+    }
+    else
+    {
+        for(int i = 0; i < n; i++)
+        {
+            a *= 2;
+        }
+    }
+    cout << m % a << endl;
+}
