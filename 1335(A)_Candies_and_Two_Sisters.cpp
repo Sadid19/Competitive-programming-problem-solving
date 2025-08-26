58bed889a6fe4e94cc790959529a91d880c58321
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-08-26 21:16:50 +0600

    Today's problem solved!

---
+++ 1335(A)_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+       ll n;
+       cin >> n;
+
+       if(n <= 2)
+       {
+           cout << 0 << endl;
+       }
+       else
+       {
+           cout << (n - 1) / 2 << endl;
+       }
+    }
+}
