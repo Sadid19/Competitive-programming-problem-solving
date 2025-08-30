88e990e79d70ebbef1291f591f53faab19e19042
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-08-31 01:28:31 +0600

    Time pass on tanguar haor!

---
+++ Codeforce Group Contest Problems/Sheet #1 (Data Type)/219158(D)_Difference.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+#include <bits/stdc++.h>
+#define ll lon long int
+using namespace std;
+int main()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    ll x = (a * b) - (c * d);
+
+    cout << "Difference = " << x << endl;
+}
