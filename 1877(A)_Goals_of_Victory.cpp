ba17ba562985262b4d8c4cc52a200211998c46a3
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-09-16 02:01:03 +0600

    Today's problem solved!

---
+++ 1877(A)_Goals_of_Victory.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int total = 0;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            int a;
+            cin >> a;
+            total += a;
+        }
+        cout << -total << endl;
+    }
+}
