33bd8695ca23b24bd66475282c90869264a2c387
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-09-17 23:48:31 +0600

    Today's problem solved!

---
+++ 1748(A)_The_Ultimate_Square.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll maxSide = (n + 1) / 2;
+
+        cout << maxSide << endl;
+    }
+}
